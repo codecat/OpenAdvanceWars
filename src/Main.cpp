2b9c9776fc16b1708c7bed050a28b7542733e4fc
Angelo Geels <spansjh@gmail.com>
2015-05-12 22:08:15 +0200

    Content folder works

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,42 @@
 #include "StdH.h"
 #include "Game.h"
 
+#ifdef _MSC_VER
+#include <direct.h>
+#else
+#include <unistd.h>
+#include <errno.h>
+#endif
+
 CRTSGame* _pGame = 0;
 
 int main()
 {
+#ifdef _MSC_VER
+	DWORD dwFlags = GetFileAttributesA("Content");
+	if(dwFlags == INVALID_FILE_ATTRIBUTES) {
+		_chdir("../.."); // bin/Debug/astra.exe
+	}
+#else
+	for(int i=0; i<3; i++) {
+		if(access("Content/", F_OK) != 0) {
+			if(errno == ENOENT || errno == ENOTDIR) {
+				chdir(".."); // bin/astra.exe
+			}
+		} else {
+			break;
+		}
+	}
+#endif
+
+	char szCurrentDirectory[1024];
+	if(getcwd(szCurrentDirectory, 1024) != NULL) {
+		printf("Current directory: %s\n", szCurrentDirectory);
+	} else {
+		printf("Working directory failure?\n");
+		return 1;
+	}
+
 	_pGame = new CRTSGame;
 	_pGame->Run();
 
